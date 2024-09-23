d68989cd57d3b24a928dc2607a282d6290898a81
asier <asier@gmail.com>
2024-09-23 10:38:47 +0200

    todos

---
+++ Ejercicios/Arrays/ej5a.php
@@ -0,0 +1,46 @@
+<HTML>
+<HEAD><TITLE> EJ1A – NUM IMPARES</TITLE></HEAD>
+<BODY>
+<?php
+    $array1=array ('Bases Datos', 'Entornos Desarrollo', 'Programación');
+    $array2=array ('Sistemas Informáticos','FOL','Mecanizado');
+    $array3=array ('Desarrollo Web ES','Desarrollo Web EC','Despliegue','Desarrollo Interfaces', 'Inglés'
+);
+    $arrayU1=array();
+    $arrayU2=array();
+    $arrayU3=array();
+
+    $arrayU1=unirSinFunciones($arrayU1,$array1);
+    $arrayU1=unirSinFunciones($arrayU1,$array2);
+    $arrayU1=unirSinFunciones($arrayU1,$array3);
+
+    $arrayU2=array_merge($array1,$array2,$array3);
+
+    array_push($arrayU3, ...$array1);
+    array_push($arrayU3, ...$array2);
+    array_push($arrayU3, ...$array3);
+
+    print ("<h2>1:</h2>");
+    imprimir($arrayU1);
+    print ("<h2>2:</h2>");
+    imprimir($arrayU2);
+    print ("<h2>3:</h2>");
+    imprimir($arrayU3);
+
+    function unirSinFunciones($res, $data){
+        foreach ($data as $value) {
+            $indice=count($res);
+            $res[$indice] = $value;
+        }
+        return $res;
+    }
+
+    function imprimir($data){
+        foreach ($data as $value) {
+            print("<p>".$value."</p>");
+        }
+    }
+
+?>
+</BODY>
+</HTML>
