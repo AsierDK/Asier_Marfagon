736abf529434691d75efa6b6f9023fee5fdb3a67
asier <asier@gmail.com>
2024-10-15 10:35:01 +0200

    ficheros

--- Ejercicios/Ficheros/fichero1.php
+++ Ejercicios/Ficheros/fichero1.php
@@ -15,52 +15,52 @@
 <?php
 if ($_SERVER["REQUEST_METHOD"] == "POST")
 {
-    function recogerDatos(){
-        $nombre = limpiar($_REQUEST['nombre']);
-        $apellido1 = limpiar($_REQUEST['ape1']);
-        $apellido2 = limpiar($_REQUEST['ape2']);
-        $fecNac = $_REQUEST['fecNac'];
-        $localidad =limpiar($_REQUEST['localidad']);
-        fichero($nombre,$apellido1,$apellido2,$fecNac,$localidad);
-    }
-
-    function limpiar($data){
-        $data = trim($data);
-        $data = stripslashes($data);
-        $data = htmlspecialchars($data);
-        return $data;
-    }
-
-    function fichero($nombre,$ape1,$ape2,$fecNac,$localidad){
-        $file = fopen('C:\\wamp64\\www\\files\\alumnos1.txt',"a") or die("No se ha encontrado el fichero");
-        $indice = 0;
-        $indice += strlen($nombre);
-        $indice = escribirFichero($file,$nombre,$indice,39);
-        $indice += strlen($ape1);
-        $indice = escribirFichero($file,$ape1,$indice,80);
-        $indice += strlen($ape2);
-        $indice = escribirFichero($file,$ape2,$indice,122);
-        $indice += strlen($fecNac);
-        $indice = escribirFichero($file,$fecNac,$indice,132);
-        $indice += strlen($localidad);
-        $indice = escribirFichero($file,$localidad,$indice,159);
-        fwrite($file,"\n");
-        fclose($file);
-
-    }
-
-    function escribirFichero($file,$campo,$indice,$limite)
-    {
-        fwrite($file,$campo);
-        while($indice < $limite)
-        {
-            fwrite($file," ");
-            $indice += 1;
-        }
-        return $indice;
-    }
-
     recogerDatos();
+}
+
+function recogerDatos(){
+    $nombre = limpiar($_REQUEST['nombre']);
+    $apellido1 = limpiar($_REQUEST['ape1']);
+    $apellido2 = limpiar($_REQUEST['ape2']);
+    $fecNac = $_REQUEST['fecNac'];
+    $localidad =limpiar($_REQUEST['localidad']);
+    fichero($nombre,$apellido1,$apellido2,$fecNac,$localidad);
+}
+
+function limpiar($data){
+    $data = trim($data);
+    $data = stripslashes($data);
+    $data = htmlspecialchars($data);
+    return $data;
+}
+
+function fichero($nombre,$ape1,$ape2,$fecNac,$localidad){
+    $file = fopen('..\\files\\alumnos1.txt',"a") or die("No se ha encontrado el fichero");
+    $indice = 0;
+    $indice += strlen($nombre);
+    $indice = escribirFichero($file,$nombre,$indice,39);
+    $indice += strlen($ape1);
+    $indice = escribirFichero($file,$ape1,$indice,80);
+    $indice += strlen($ape2);
+    $indice = escribirFichero($file,$ape2,$indice,122);
+    $indice += strlen($fecNac);
+    $indice = escribirFichero($file,$fecNac,$indice,132);
+    $indice += strlen($localidad);
+    $indice = escribirFichero($file,$localidad,$indice,159);
+    fwrite($file,"\n");
+    fclose($file);
+
+}
+
+function escribirFichero($file,$campo,$indice,$limite)
+{
+    fwrite($file,$campo);
+    while($indice < $limite)
+    {
+        fwrite($file," ");
+        $indice += 1;
+    }
+    return $indice;
 }
 ?>
 
