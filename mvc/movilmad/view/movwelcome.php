5119b4df33111439a1029e561039b68ddbddff89
asier <asier@gmail.com>
2025-01-15 12:12:12 +0100

    movilmad

--- mvc/movilmad/view/movwelcome.php
+++ mvc/movilmad/view/movwelcome.php
@@ -24,7 +24,7 @@
 		
        <!--Formulario con botones -->
 	
-		<input type="button" value="Alquilar Vehículo" onclick="window.location.href='view/movalquilar.php'" class="btn btn-warning disabled">
+		<input type="button" value="Alquilar Vehículo" onclick="window.location.href='funciones_movalquilar.php'" class="btn btn-warning disabled">
 		<input type="button" value="Consultar Alquileres" onclick="window.location.href='view/movconsultar.php'" class="btn btn-warning disabled">
 		<input type="button" value="Devolver Vehículo" onclick="window.location.href='view/movdevolver.php'" class="btn btn-warning disabled">
 		</br></br>
