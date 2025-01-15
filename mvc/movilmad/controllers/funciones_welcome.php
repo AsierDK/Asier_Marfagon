ab125984c55b4b09196ac7bdd5ad703751dcd25f
asier <asier@gmail.com>
2025-01-15 12:00:36 +0100

    movilmad

--- mvc/movilmad/controllers/funciones_welcome.php
+++ mvc/movilmad/controllers/funciones_welcome.php
@@ -1,6 +1,7 @@
 <?php
 require_once 'funciones_comunes.php';
 require_once 'funciones_session.php';
+iniciarSession();
 $nombre=devolvernombre();
 $id=devolverID();
 require_once '../view/movwelcome.php';
