6e5b4811e848c8db7fd8214e3406b24590842909
asier <asier@gmail.com>
2025-01-22 10:07:47 +0100

    mvc

--- mvc/movilmad/models/func_db_alquilar.php
+++ mvc/movilmad/models/func_db_alquilar.php
@@ -17,6 +17,22 @@ function saberVehiculosDisponibles()
         $conn=null;
         return $resultado;
 }
+function saberVehiculosAlquilados($id)
+{
+    try
+    {
+        $stmt = $GLOBALS["conn"]->prepare("SELECT count(*) as alquilados from ralquileres where idcliente = :idcliente and fecha_devolucion is null and preciototal is null and fechahorapago is null");
+        $stmt->bindParam(':idcliente', $id);
+        $stmt -> execute();
+        $stmt->setFetchMode(PDO::FETCH_ASSOC);
+        $resultado=$stmt->fetchAll();
+    }
+    catch(PDOException $e)
+    {
+        echo "Error: " . $e->getMessage();
+    }
+    return $resultado[0]["alquilados"];
+}
 
 function realizarAlquiler($cesta,$id)
 {
