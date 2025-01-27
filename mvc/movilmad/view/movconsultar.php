8ea140cb788e05f0fb1006360651b991b4327715
asier <asier@gmail.com>
2025-01-27 20:05:29 +0100

    mvc

--- mvc/movilmad/view/movconsultar.php
+++ mvc/movilmad/view/movconsultar.php
@@ -30,9 +30,9 @@
 			 Fecha Hasta: <input type='date' name='fechahasta' value='' size=10 placeholder="fechahasta" class="form-control"><br><br>
 				
 		<div>
-			<input type="submit" value="Consultar" name="Consultar" class="btn btn-warning disabled">
+			<input type="submit" value="Consultar" name="consultar" class="btn btn-warning disabled">
 		
-			<input type="submit" value="Volver" name="Volver" class="btn btn-warning disabled">
+			<input type="submit" value="Volver" name="volver" class="btn btn-warning disabled">
 		
 		</div>		
 	</form>
