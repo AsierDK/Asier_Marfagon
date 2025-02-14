6571aa5fc15978939eb64ad40e342fef2922bc44
asier <asier@gmail.com>
2025-02-14 13:08:34 +0100

    appmusic

---
+++ mvc/appmusic/index.php
@@ -0,0 +1,3 @@
+<?php
+require_once 'controllers/funciones_login.php';
+?>
--- mvc/movilmad/tpv/controllers/controller_cerrarS.php
+++
@@ -1,7 +0,0 @@
-<?php
-require_once "funciones_session.php";
-
-eliminarSessionSinRedireccion();
-
-header("Location: ../index.php");
-?>
